223dcf0ddc5fac9c044ea896c37ac66ae5318685
Edmund <edmund.adjei635@gmail.com>
2022-05-25 18:08:34 -0400

    commit tree 10dd9d4c85e16b9c203dc9e52c7d5ff0fc5c19ca

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,6 @@ const sinon = require('sinon');
 
 describe("index.js", () => {
   let input;
-
   beforeEach(function() {
     input = document.getElementById('input');
     sinon.spy(input, 'addEventListener');
